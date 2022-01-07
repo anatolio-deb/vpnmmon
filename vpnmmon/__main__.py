32935b6d5139e5756df9d69c89240ce4ab811d53
Анатолий Никифоров <nikiforova693@gmail.com>
2022-01-07 06:47:53 +0500

    log added

--- vpnmmon/__main__.py
+++ vpnmmon/__main__.py
@@ -4,39 +4,55 @@ import threading
 from vpnmauth import VpnmApiClient
 
 
-def traceroute(node_id: int, host: str) -> None:
-    global NODES_AVAILABLE
-    proc = subprocess.run(["traceroute", host], capture_output=True, check=True)
-    output = proc.stdout.decode()
-    if output.endswith("30  * * *") or len(output.split("\n")) <= 3:
-        print(f"Node id{node_id} is unavailable")
-    else:
-        print(f"Node id{node_id} is available")
-        NODES_AVAILABLE += 1
+class Monitor:
+    total_available = 0
+    log_path = "/tmp/vpnmmon.log"
+    lock = threading.Lock()
+    threads: threading.Thread = []
+
+    def __init__(self) -> None:
+        self.client = VpnmApiClient()
+        print("Logged in")
+        self.nodes = self.client.get_nodes()
+        print(f"{len(self.nodes)} nodes recieved")
+
+    def traceroute(self, node_id: int, host: str) -> None:
+        proc = subprocess.run(["traceroute", host], capture_output=True, check=True)
+        output = proc.stdout.decode()
+        if output.endswith("30  * * *\n") or len(output.split("\n")) <= 3:
+            print(f"Node id{node_id} is unavailable")
+        else:
+            print(f"Node id{node_id} is available")
+            self.total_available += 1
+
+        self.lock.acquire()
+
+        with open(self.log_path, "a", encoding="utf-8") as file:
+            file.write(output)
+
+        self.lock.release()
+
+    def run(self):
+        for node in self.nodes:
+            thread = threading.Thread(
+                target=self.traceroute,
+                args=(
+                    node["id"],
+                    node["server"].split(";")[0],
+                ),
+            )
+            thread.start()
+            print(f"Tracerouting node id{node['id']} in thread {thread.ident}")
+            self.threads.append(thread)
+
+        for thread in self.threads:
+            thread.join()
+
+        print("Availability check completed")
+        print(f"{self.total_available}/{len(self.nodes)} nodes available")
+        print(f"Full log can be found at {self.log_path}")
 
 
 if __name__ == "__main__":
-    NODES_AVAILABLE = 0
-    threads: threading.Thread = []
-    client = VpnmApiClient()
-    print("Logged in")
-    nodes = client.get_nodes()
-    print(f"{len(nodes)} nodes recieved")
-
-    for node in nodes:
-        thread = threading.Thread(
-            target=traceroute,
-            args=(
-                node["id"],
-                node["server"].split(";")[0],
-            ),
-        )
-        thread.start()
-        print(f"Tracerouting node id{node['id']} in thread {thread.ident}")
-        threads.append(thread)
-
-    for thread in threads:
-        thread.join()
-
-    print("Availability check completed")
-    print(f"{NODES_AVAILABLE}/{len(nodes)} nodes available")
+    monitor = Monitor()
+    monitor.run()
