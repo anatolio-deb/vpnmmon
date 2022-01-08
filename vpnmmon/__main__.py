7e5768c83b0ffebb39c40244805ccc58108c6f5f
Анатолий Никифоров <nikiforova693@gmail.com>
2022-01-08 21:20:57 +0500

    bug fixes

--- vpnmmon/__main__.py
+++ vpnmmon/__main__.py
@@ -36,7 +36,7 @@ class Monitor:
             self.lock.acquire()
 
             with open(self.log_path, "a", encoding="utf-8") as file:
-                file.write("\n".join(result.append("\n")))
+                file.write("\n".join(result) + "\n")
 
             self.lock.release()
 
